04a38f2a4742a719f6ac6455edd0d136e95d6859
Jonas Machado <jonasmachado.ti@gmail.com>
2023-12-11 16:30:28 -0300

    :recycle: refactor: Put the AppNavigator in App.js

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
-    <>
-      
-    </>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
