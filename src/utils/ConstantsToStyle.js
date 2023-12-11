0f55f36017fa59cce7f203b1adc3acbe15752e02
Jonas Machado <jonasmachado.ti@gmail.com>
2023-12-11 19:48:24 -0300

    :sparkles: feat: Create a ConstantsToStyle file for managing fonts and default colors.

---
+++ src/utils/ConstantsToStyle.js
@@ -0,0 +1,6 @@
+
+export const TEXT_COLOR = "#000000";  
+export const FONT_FAMILY = "Arial";
+export const PRIMARY_COLOR = "#424FEE"; 
+export const SECONDARY_COLOR = "#979797";
+export const BACKGROUND_COLOR = "#F0F3FA";
